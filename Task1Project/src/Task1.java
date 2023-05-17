caa46ef37c6f48bd5c8d98e430a7be97e5260683
sonalbhawsar <bhawsar.sonal@gmail.com>
2023-05-17 11:12:32 -0500

    adding the project.

---
+++ Task1Project/src/Task1.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author sonalbhawsar
+ *
+ */
+public class Task1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+
+	}
+
+}
